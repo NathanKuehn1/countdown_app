85fc9f9f7b240bde8a33096081b4f98e220e12b6
Nathan <linkingitup@outlook.com>
2018-04-05 11:41:02 -0600

    Generate buttton

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import DatePicker from 'react-datepicker';
-import moment from 'moment'
+import moment from 'moment';
 
 class Birthdayform extends Component {
 
@@ -9,9 +9,11 @@ class Birthdayform extends Component {
         super(props);
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleGenerate.bind(this)
 
         this.state = {
-            startDate: moment()
+            startDate: moment(),
+            formCompleted: false
         }
 
     }
@@ -19,10 +21,16 @@ class Birthdayform extends Component {
     handleChange(date) {
         this.setState({
             startDate: date
+        });
+    }
+
+
+    handleGenerate() {
+        this.setState({
+            formCompleted: true
         })
     }
 
-    
     render() {
         return (
             <div>
@@ -30,7 +38,8 @@ class Birthdayform extends Component {
                 <DatePicker
                     selected={this.state.startDate}
                     onChange={this.handleChange}
-                    />
+                 />
+                <a onClick={this.handleGenerate}>Generate Countdown</a>
             </div>
         )
     }
